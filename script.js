edc3e2fa533f8c0bd7cb58c2f2b2569e09e6909b
erdong <zhiguangphoenix@gmail.com>
2023-03-11 23:46:48 +0800

    feat: toggle文档

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ submit_btn.addEventListener('click', function () {
     modal.style.display = 'none'
 })
 bar_btn.addEventListener('click', function () { 
+    /**
+     * toggle这个方法的用法可以参考文档https://developer.mozilla.org/en-US/docs/Web/API/DOMTokenList/toggle，简单来说就是：
+     * The toggle() method of the DOMTokenList interface removes an existing token from the list and returns false.
+     * If the token doesn't exist it's added and the function returns true.
+     */
     document.body.classList.toggle('show-nav')
 })
 
